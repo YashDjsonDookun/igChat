105dadc99969d2f6243dc929c2b8bb3c95c90a16
Yash Djson Dookun <dookun88@gmail.com>
2020-05-07 23:20:06 +0400

    Kinda working - Kinda not working

--- app/igchat.py
+++ app/igchat.py
@@ -5,54 +5,6 @@ import tkinter.ttk as ttk
 import time
 from tkinter.messagebox import showinfo
 from selenium import webdriver
-#
-# win = Tk()
-# win.title('IG Chat')
-# win.geometry('300x500')
-#
-# def Login():
-#     username_automation = username.get()
-#     password_automation = password.get()
-#
-#     if username_automation == "" or password_automation == "":
-#         showinfo("IG Chat", "Username and Password need to be filled!")
-#     else:
-#         driver = webdriver.Chrome()
-#         driver.get("https://www.instagram.com/")
-#         time.sleep(3)
-#
-#         user = driver.find_element_by_xpath(
-#             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')
-#         user.send_keys(username_automation)
-#         time.sleep(3)
-#
-#         passwd = driver.find_element_by_xpath(
-#             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')
-#         passwd.send_keys(password_automation)
-#
-#         login = driver.find_element_by_xpath(
-#             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button')
-#         login.click()
-#         time.sleep(3)
-#
-#         turnOnNotif = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]')
-#         turnOnNotif.click()
-#
-# username_label = ttk.Label(win, text="Username:", foreground="blue", font=("", 12))
-# username_label.grid(row=0, column=0)
-# username = ttk.Entry(win, width=20)
-# username.grid(row=0, column=1)
-#
-# password_label = ttk.Label(win, text="Password:", foreground="blue", font=("", 12))
-# password_label.grid(row=1, column=0)
-# password = ttk.Entry(win, show="*", width=20)
-# password.grid(row=1, column=1, padx=15)
-#
-# button = ttk.Button(win, text="Login", command=Login)
-# button.grid(row=2, column=0, columnspan=2)
-#
-# win.mainloop()
-
 import tkinter as tk
 
 class IgChat(tk.Frame):
@@ -81,7 +33,7 @@ class IgChat(tk.Frame):
             # self.button = tk.Button(self.win, text="Login").pack()
             # self.button.grid(row=2, column=0, columnspan=2)
 
-            tk.Button(self.win, text="Destroy all widgets in this frame!", command= self.clear_window).pack()
+            tk.Button(self.win, text="Login", command= self.clear_window).pack()
 
     def clear_window(self):
         # self.win.destroy()
